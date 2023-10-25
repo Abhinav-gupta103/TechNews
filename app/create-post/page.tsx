e62c213cc8a8cb5d61631cc4e73db2f0df6acabd
Abhinav-gupta103 <ag0537269@gmail.com>
2023-10-26 00:48:15 +0530

    routes Protecting and profile image functionalities

--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -1,7 +1,11 @@
 import CreatePostForm from "@/components/CreatePostForm";
-import React from "react";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/sign-in");
   return <CreatePostForm />;
 };
 
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,9 +1,12 @@
 import SignInBtns from "@/components/SignInBtns";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-const page = () => {
-	return (
-		<SignInBtns />
-	)
-}
+const page = async () => {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/dashboard");
+  return <SignInBtns />;
+};
 
 export default page;
